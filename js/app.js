e680350bc4d7809949b177fdf14b02a309bba036
Jean-Christophe BEGUE <jc.begue@devisubox.com>
2018-04-01 16:49:55 +0200

    use a bootstrap template from bootswatch

--- js/app.js
+++ js/app.js
@@ -1,9 +1,2 @@
 import 'bootstrap';
-
-const yep = () => {
-    console.log("yep");
-}
-
-const es6_variable = 'salut';
-console.log(`running ${es6_variable}`);
-yep()
+import "app.scss"
--- webpack.config.js
+++ webpack.config.js
@@ -2,33 +2,41 @@ const webpack = require('webpack');
 const path = require('path');
 
 const config = {
-  context: __dirname,
-  entry: {
-    main: [
-      './js/app.js',
-    ],
-  },
-  // Render source-map file for final build
-  devtool: 'source-map',
-  // output config
-  output: {
-    path: path.resolve(__dirname, './public/'), // Path of output file
-    filename: 'app.js', // Name of output file
-  },
-  plugins: [],
-  resolve: {
-    modules: [path.join(__dirname, 'src', 'main', 'assets', 'js'), "node_modules"],
-    extensions: ['.js'],
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-    ],
-  },
+    context: __dirname,
+    entry: {
+	main: [
+	    './js/app.js',
+	],
+    },
+    // Render source-map file for final build
+    devtool: 'source-map',
+    // output config
+    output: {
+	path: path.resolve(__dirname, './public/'), // Path of output file
+	filename: 'app.js', // Name of output file
+    },
+    plugins: [],
+    resolve: {
+	modules: [path.join(__dirname, 'styles'), "node_modules"],
+	extensions: ['.js'],
+    },
+    module: {
+	rules: [
+	    {
+		test: /\.js$/,
+		exclude: /node_modules/,
+		loader: 'babel-loader',
+	    }, {
+		test: /\.scss$/,
+		use: [{
+		    loader: "style-loader" // creates style nodes from JS strings
+		}, {
+		    loader: "css-loader" // translates CSS into CommonJS
+		}, {
+		    loader: "sass-loader" // compiles Sass to CSS
+		}],
+	    },
+	]}
 };
 
 module.exports = config;
