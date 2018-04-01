87c34c558ae945645fee3f5536a58218b09dbbb1
Jean-Christophe BEGUE <jc.begue@devisubox.com>
2018-04-01 08:15:50 +0200

    fix tests

--- tests/main.rs
+++ tests/main.rs
@@ -5,10 +5,12 @@ extern crate serde_json;
 extern crate stammw_blog;
 extern crate rocket;
 extern crate rocket_contrib;
+extern crate regex;
 
 use rocket::local::{Client, LocalResponse};
 use rocket::http::Method::*;
 use rocket::http::{Status, ContentType};
+use regex::Regex;
 
 macro_rules! dispatch {
     ($method:expr, $path:expr, $test_fn:expr) => ({
@@ -40,6 +42,10 @@ fn create_post() {
         format!("body={}&title={}", "Body", "Title"),
         |_, response: LocalResponse| {
             assert_eq!(response.status(), Status::SeeOther);
+            let excepted_url = Regex::new(r"^/post/\d+$").unwrap();
+            let location = response.headers().get("Location").last(); 
+            assert!(location.is_some());
+            assert!(excepted_url.is_match(location.unwrap()));
         }
     );
 }
@@ -55,9 +61,22 @@ fn create_post_with_empty_title_fails() {
 }
 
 #[test]
-#[ignore]
 fn gets_one_post() {
-    dispatch!(Get, "/post/1", |_, response: LocalResponse| {
-        assert_eq!(response.status(), Status::Ok);
-    });
+    let mut post_id = -1;
+    dispatch_post!("/post/new", format!("body={}&title={}", "Body", "Title"),
+        |_, response: LocalResponse| {
+            assert_eq!(response.status(), Status::SeeOther);
+            let excepted = Regex::new(r"^/post/(\d+)$").unwrap();
+            let location = response.headers().get("Location")
+                .last().unwrap(); 
+            post_id = excepted.captures(location).unwrap()
+                       .get(1).expect("post id not found in 'Location'")
+                       .as_str().parse::<i32>().unwrap();
+        }
+    );
+    dispatch!(Get, format!("/post/{}", post_id),
+        |_, response: LocalResponse| {
+            assert_eq!(response.status(), Status::Ok);
+        }
+    );
 }
