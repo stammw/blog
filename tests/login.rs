cb47710c738a5571ba08e229a272c1c20730cc3f
Jean-Christophe BEGUE <jean-Christophe.begue@fraudbuster.mobi>
2018-07-18 13:01:34 +0200

    move login tests to thier own module

---
+++ tests/login.rs
@@ -0,0 +1,48 @@
+extern crate stammw_blog;
+extern crate rocket;
+extern crate rocket_contrib;
+
+use rocket::local::Client;
+use rocket::http::{Status, ContentType};
+
+use stammw_blog::auth::UserToken;
+
+fn check_login(body: &str, location: &str, success: bool) {
+    let rocket = stammw_blog::rocket();
+    let client = Client::new(rocket).unwrap();
+
+    let response = client.post("/login")
+        .header(ContentType::Form)
+        .body(body)
+        .dispatch();
+
+    assert_eq!(response.status(), Status::SeeOther);
+    assert_eq!(response.headers().get_one("Location"), Some(location));
+    let cookie = response.headers().get_one("Set-Cookie");
+    if success {
+        assert!(cookie.is_some());
+        assert_eq!(&cookie.unwrap()[..5], "user=");
+    } else {
+        assert!(cookie.is_none());
+    }
+}
+
+#[test]
+fn login_success() {
+    check_login("email=user1@email.test&password=password1", "/", true);
+}
+
+#[test]
+fn login_fails_wrong_password() {
+    check_login("email=user1@email.test&password=wrong_password", "/login", false);
+}
+
+#[test]
+fn login_fails_empty_password() {
+    check_login("email=user1@email.test&password=", "/login", false);
+}
+
+#[test]
+fn login_fails_empty_email() {
+    check_login("email=&password=password1", "/login", false);
+}
