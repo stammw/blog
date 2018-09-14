326bf795fe6be5f04c94832ac66b0fcf8a0738d5
Jean-Christophe BEGUE <jean-Christophe.begue@fraudbuster.mobi>
2018-09-14 16:26:04 +0200

    Fix sequence problems with test data

--- tests/scripts/test_db.sql
+++ tests/scripts/test_db.sql
@@ -1,10 +1,12 @@
-TRUNCATE TABLE users;
+TRUNCATE TABLE posts, users;
+
 INSERT INTO users VALUES
 (1, 'user1', 'user1@email.test', 'DrlvHeS4M5/iIE37xDZTy7uzBiTKSmoNblzNH2PFRU4='),
 (2, 'user2', 'user2@email.test', 'password2'),
 (3, 'user3', 'user3@email.test', 'password3');
 
-TRUNCATE TABLE posts;
+ALTER SEQUENCE users_id_seq RESTART WITH 4;
+
 INSERT INTO posts
 (id, user_id, title, slug, body, published, creation_date)
 VALUES
@@ -12,6 +14,8 @@ VALUES
 (2, 1, 'title2: The second post', 'title2-the-second-post', 'body2', true, TIMESTAMP '2018-08-20 10:23:54'),
 (3, 1, 'title3', 'title3', '# body3', false, TIMESTAMP '2018-08-21 10:23:54'),
 (45, 1, 'title45', 'title45', '# body45', false, TIMESTAMP '2018-08-19 10:23:54');
+
+ALTER SEQUENCE posts_id_seq RESTART WITH 46;
 
 -- SELECT * FROM users;
 -- SELECT * FROM posts;
